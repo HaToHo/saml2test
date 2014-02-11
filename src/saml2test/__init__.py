839a9340b4de914572589b9d8ff48025d41f3133
Hans Hörberg <hans.horberg@umu.se>
2014-02-11 10:33:24 +0100

    Bugg fix

--- src/saml2test/__init__.py
+++ src/saml2test/__init__.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 __author__ = 'rolandh'
 
-JSON_DUMPS_ARGS = {"sort_keys": True}
+JSON_DUMPS_ARGS = {"indent": 4, "sort_keys": True}
 
 
 class FatalError(Exception):
